0592b54cbfc9297d36a3d83472ecb59ce9cd3451
janvi12940 <pateljanvi54970@gmail.com>
2026-04-15 12:23:14 +0530

    Update vite.config.js

--- result management system/student-result-system/frontend/vite.config.js
+++ result management system/student-result-system/frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/Result-Management-System/",   // 👈 ADD THIS LINE
   plugins: [react()],
   server: {
     proxy: {
